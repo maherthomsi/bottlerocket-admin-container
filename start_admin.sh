ce5fcc4a1b39201361730ed34f7122ba66974b69
maherthomsi <maherhom@amazon.com>
2025-06-19 22:48:28 +0000

    added journal sharing and cgroup compatability

--- start_admin.sh
+++ start_admin.sh
@@ -236,29 +236,16 @@ enable_systemd_services
 
 systemd_options=()
 
-# cgroup v2 compatibility crimes: systemd 219 in the admin container only
-# supports cgroup v1.
-if [[ $(findmnt -n -o FSTYPE /sys/fs/cgroup) = cgroup2 ]]; then
-    # Mount an extra cgroup v1 hierarchy for use by systemd in the admin
-    # container. Vanilla systemd is hard-wired to look for it at /sys/fs/cgroup
-    # but that path is already taken by the host's proper cgroup hierarchy.
-    # Mounting a cgroup v1 hierarchy here might confuse the host and the systemd
-    # documentation advises against manually interfering with anything in that
-    # path. Therefore, mount the extra hierarchy elsewhere and tell systemd in
-    # the admin container where to look. Requirements:
-    #
-    #   1. The base must be a mount point.
-    #   2. The base must contain another mounted cgroup file system named "systemd".
-    #
-    # In either case there is no need to populate the file systems with any
-    # actual cgroup controllers, since the host is expected to manage processes.
-    readonly cgroup_base=/.bottlerocket/admin-container-cgroup
-    mkdir -p "${cgroup_base}"
-    mount -t tmpfs -o nosuid,nodev,noexec tmpfs "${cgroup_base}"
-    mkdir -p "${cgroup_base}"/systemd
-    mount -o remount,ro "${cgroup_base}"
-    mount -t cgroup -o none,name=systemd cgroup "${cgroup_base}"/systemd
-    systemd_options+=( --cgroup-base "${cgroup_base}" )
+# Set up journal linking to host journal
+readonly host_journal_socket="/.bottlerocket/rootfs/run/systemd/journal/socket"
+readonly container_journal_socket="/run/systemd/journal/socket"
+if [[ -S "${host_journal_socket}" ]]; then
+  # Create directory and empty socket file before mounting and binding the
+  # container journal to the host bottlerocket journal
+  mkdir -p /run/systemd/journal
+  touch "${container_journal_socket}"
+  mount --bind "${host_journal_socket}" "${container_journal_socket}"
+  log "Journal linking to host journal enabled"
 fi
 
 # Persuade systemd that it's OK to run as a user manager.
